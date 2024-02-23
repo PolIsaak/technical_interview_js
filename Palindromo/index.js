ea29f6441d7ca338e05a6b06f4e0e40f38e92390
PolIsaak <paulisaak.dev@outlook.com>
2024-02-23 16:33:06 -0700

    add solution to encontrarMayor

--- Palindromo/index.js
+++ Palindromo/index.js
@@ -1,14 +1,11 @@
 /*
-? Problema 2: Palindromo
+# PROBLEMA 2: Palindromo
+  Escribe una función llamada 'esPalindromo' que recibe una cadena de texto y determine si es un palindromo o no.
+  Un palindromo es una palabra, frase, número o cualquier secuencia que se lee igual hacia adelante que hacia atrás, 
+  ignorando espacios y mayúsculas/minúsculas
 
-Escribe una función llamada 'esPalindromo' que recibe una cadena de texto y determine si es un palindromo o no.
-Un palindromo es una palabra, frase, número o cualquier secuencia que se lee igual hacia adelante que hacia atrás, 
-ignorando espacios y mayúsculas/minúsculas
-*/
-
-/*
-* SOLUCIÓN 1
-? Uso de sentencia if para la evaluación de las palabras
+# SOLUCIÓN 1
+- Uso de sentencia if para la evaluación de las palabras
 */
 function esPalindromo(str){
   const lowerRegStr = str.toLowerCase().replace(/ /g, '');
@@ -28,8 +25,8 @@ function reverseStr(strToReverse){
 }
 
 /*
-* SOLUCIÓN 2
-? Simplifica la función 'esPalindromo'
+# SOLUCIÓN 2
+- Simplifica la función 'esPalindromo'
 */
 function esPalindromoRefactorized(str){
   const lowerRegStr = str.toLowerCase().replace(/ /g, '');
@@ -44,7 +41,7 @@ function reverseStr(strToReverse){
 }
 
 /*
-* More info..
+# MAS INFO..
 ? https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
 ? https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
 ? https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
